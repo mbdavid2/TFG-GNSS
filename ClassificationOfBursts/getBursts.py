6d4a0bd991d372fa80a23c2682e0daf0b58c6af3
Unknown <mbdavid297@gmail.com>
2019-02-25 19:31:30 +0100

    Burst classification done, table results

--- ClassificationOfBursts/getBursts.py
+++ ClassificationOfBursts/getBursts.py
@@ -1,11 +1,9 @@
 from bs4 import BeautifulSoup
 import urllib.request
 import subprocess
+import math
 
-listGRBs = []
-sortedGRBs = []
-
-def loadContent():
+def loadContentSwift():
 	## Get page ##
 	url = "https://swift.gsfc.nasa.gov/archive/grb_table.html/quickview/"
 	response = urllib.request.urlopen(url)
@@ -16,13 +14,13 @@ def loadContent():
 	# print(soup)
 	return soup
 
-def printList():
-	print("newRA	||	newDec	||	totalDifference")
-	for x in listGRBs:
+def prettyPrintList(listToPrint):
+	print(["Name", "TotalScore", "[Year, Month, Day, DecTime]", "dotProduct", "Ra", "Dec", "SunRa", "SunDec", "UVOT", "BAT"])
+	for x in listToPrint:
 		print(x)
 
-def traverseTable(soup):
-	global listGRBs
+def traverseTableAndSort(soup, sort):
+	listGRBs = [["Name", 0, "Date", 0, "Ra", "Dec", "SunRa", "SunDec", "UVOT magnitude", "BAT fluence"]]
 	table_body = soup.find('table', attrs={'class':'grbtable'})
 	rows = table_body.find_all('tr')
 	for row in rows:
@@ -30,12 +28,9 @@ def traverseTable(soup):
 		cols = [ele.text.strip() for ele in cols]
 		oneGRB = [ele for ele in cols if ele]
 		if oneGRB:
-			infoGRB = computeNecessaryInfo(oneGRB[0:7])
-			listGRBs.append(infoGRB) # Only the necessary data for each GRB¿?¿?¿
-	listGRBs = listGRBs[0:4]
-	printList()
-	
-	# print(listGRBs[1][3], listGRBs[2][3])
+			infoGRB = computeNecessaryInfo(oneGRB)
+			insertNewGRB(listGRBs, infoGRB, sort)
+	return listGRBs
 
 def computeNecessaryInfo(dataGRB):
 	# Gregorian date
@@ -43,78 +38,124 @@ def computeNecessaryInfo(dataGRB):
 	year = "20" + date[0:2]
 	month = date[2:4]
 	day = date[4:6]
-	date = [year, month, day]
 
 	# UT Time
 	time = dataGRB[1]
 	decTime = int(time[0:2]) + int(time[3:5])/60
 
+	date = [year, month, day, decTime]
+
 	# BAT RA, "clean" the number
 	ra = dataGRB[3]
 	if (ra[0] == "n"):
-		return 0
-	ra = (ra.split(":")[0])
-	newRA = float(ra[0:len(ra)-2])
+		newRA = 0
+	else:
+		ra = (ra.split(":")[0])
+		newRA = float(ra[0:len(ra)-2])
 
 	# BAT Dec, "clean" the number
 	dec = dataGRB[4]
 	if (dec[0] == "n"):
-		return 0
-	dec = (dec.split(":")[0])
-	if (dec[0] == "-"):
-		newDec = float(dec[0:len(dec)-3])
-		# print("Alert!",)
+		newDec = 0
 	else:
-		newDec = float(dec[0:len(dec)-2])
+		dec = (dec.split(":")[0])
+		if (dec[0] == "-"):
+			newDec = float(dec[0:len(dec)-3])
+		else:
+			newDec = float(dec[0:len(dec)-2])
 		
-
 	# Compute the position of the sun at this date
 	sunPosition = planetsV2(year, month, day, decTime)
 
-	# Compare the results of the sun to those of the burst
-	difRA = abs(sunPosition[0] - newRA)
-	difDec = abs(sunPosition[1] - newDec)
-	totalDifference = difRA + difDec
+	# Compare the results of the sun to those of the burst, dotProduct
+	scorePos = scorePosition(sunPosition[0], sunPosition[1], newRA, newDec)
 
 	# Take into consideration the "power" as well
+	scorePow = scorePower(dataGRB[14], dataGRB[6])
 
-	# it shouldn't return "results", first 
-	return [newRA, newDec, totalDifference]
+	# Function to calculate power: add both¿?¿ (if we multiply sometimes = 0)
+	totalScore = scorePos + scorePow
+
+	# Return score and parameters
+	result = [dataGRB[0], totalScore, date, scorePos, newRA, newDec, sunPosition[0], sunPosition[1], dataGRB[14], dataGRB[6]]
+	# print(result)
+	return result
+
+def scorePower(magnitudeUVOT, fluenceBAT):
+	# Clean both parameters (strings) and equal to 0 if n/a
+	if magnitudeUVOT[0] != "n":
+		magnitudeUVOT = float((magnitudeUVOT)[2:len(magnitudeUVOT)-1])
+	else:
+		magnitudeUVOT = 0
+
+	if fluenceBAT[0] == "n":
+		fluenceBAT = 0
+	else:
+		fluenceBAT = float(fluenceBAT)
+
+	# Order by magnitudeUVOT, if nonexistent, order by fluenceBAT
+	# print(magnitudeUVOT, fluenceBAT)
+	if magnitudeUVOT != 0:
+		return magnitudeUVOT
+	else:
+		return fluenceBAT
+	
+	# Another possibility: combination of both:
+	# return magnitudeUVOT + fluenceBAT
+	
+def scorePosition(sunRa, sunDec, ra, dec):
+	# If Ra and/or Dec are n/a, return 0, else, compute the dotProduct
+	if ra == 0 or dec == 0:
+		return 0
+
+	# Compute unit vectors
+	modSun = math.sqrt(sunRa*sunRa + sunDec*sunDec)
+	modGRB = math.sqrt(ra*ra + dec*dec)
+	
+	unitVecSun = list(map((lambda x: x / modSun), [sunRa, sunDec]))
+	unitVecGRB = list(map((lambda x: x / modGRB), [ra, dec]))
+
+	# Compute dot product/scalar product: "angle" between the Sun and the GRB
+	dotProduct = unitVecSun[0]*unitVecGRB[0] + unitVecSun[1]*unitVecGRB[1]
+	return dotProduct
 
 def planetsV2(year, month, day, decTime):
-	# bashCommand = 'echo "test1" "test2" "test3" > output.file'
-	# process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
-	# output, error = process.communicate()
-	results = [238, 45, 23]
+	bashCommand = ("echo \"" 
+				+ year + " " 
+				+ month + " " 
+				+ day + " " 
+				+ str(decTime) + " "
+				+ "0 0 Sun\""
+				+ " | ./a.out")
+	results = subprocess.check_output(bashCommand, shell=True)
+	results = results.split()[0:2]
+	results = list(map(float, results))
 	return results
-
-def isBetter(newElem, elem):
-	"""Returns true if newElem is better than elem"""
-	if not elem:
-		return True
-	info = 0
-	results = runPlanetsV2(info)
-
-	# Results is the angle with the Sun, newElem[]
 	
-
-
-
-def sortingFunction(array, newEle):
-	for index, elem in enumerate(array):
-		if isBetter(newElem, elem):
-			array.insert(index,newEle)
+def insertNewGRB(listGRBs, newEle, sort):
+	# print(newEle)
+	for index, elem in enumerate(listGRBs):
+		# print(elem)
+		if sort == 0:
+			if elem[1] <= newEle[1]:
+				listGRBs.insert(index,newEle)
+				break
+		else:
+			if elem[3] <= newEle[3]:
+				listGRBs.insert(index,newEle)
+				break
+		if index == len(listGRBs)-1:
+			listGRBs.insert(index,newEle)
 			break
-	return array
 	
 def main():
-	soup = loadContent()
-	traverseTable(soup)
-	# print(listGRBs)
-
-	for newGRB in listGRBs:
-		sortingFunction(sortedGRBs, newGRB)
-
-
+	ans = input("Sort only by angle? [y/n]: ")
+	if ans == "y" or ans == "Y":
+		sort = 1
+	else:
+		sort = 0
+	soup = loadContentSwift()
+	listGRBs = traverseTableAndSort(soup, sort)
+	prettyPrintList(listGRBs)
 
 main()
