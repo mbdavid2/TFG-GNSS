03a2ca2f07a2df26ea4eb20ee0df8a55216874cd
David <mbdavid297@gmail.com>
2019-04-14 10:21:26 +0200

    [C] Decreasing steps works with steps as integers, performance improved

--- Code/SunPositionAlgorithm/3-unknownSource/traverseGlobe/TraverseGlobe.cc
+++ Code/SunPositionAlgorithm/3-unknownSource/traverseGlobe/TraverseGlobe.cc
@@ -9,7 +9,16 @@ using namespace std::chrono;
 //Structs and typedefs
 struct possibleSunInfo {
 	double coefficient;
+	int ra;
+	int dec;
 	string location;
+};
+
+struct searchRange {
+	int lowerRa;
+	int upperRa;
+	int lowerDec;
+	int upperDec;
 };
 
 typedef high_resolution_clock::time_point clockTime;
@@ -52,24 +61,38 @@ void chronoStart() {
 void chronoEnd() {
 	clockTime now = high_resolution_clock::now();
 	printExecutionTime (startTime, now);
-	// auto duration = duration_cast<microseconds>(now - startTime).count();
-    // cout << duration;
 }
 
-void considerPossibleSuns(int step) {
-	cout << endl << "[C++ -> Fortran: Computing Person coefficients | Possible Suns with step=" + to_string(step) + "]" << endl; // for possible Suns | Epoch = " + to_string(epoch) + "]" << endl;
+void printCorrelationResults(possibleSunInfo bestSun) {
+	cout << endl << "[Results]" << endl;
+	cout << "   -> Largest correlation coefficient: " << bestSun.coefficient << endl;
+	cout << "   -> Estimated Sun's location: " << bestSun.location << endl;
+}
+
+void printCorrelationParameters(int step, searchRange range){
+	cout << endl << "[C++ -> Fortran: Computing Person coefficients]" << endl; 
+	cout << "[Parameters]" << endl; 
+	cout << "   -> Possible Suns with step = " + to_string(step) << endl;
+	cout << "   -> Right ascension range = [" + to_string(range.lowerRa) + ", " + to_string(range.upperRa) + "]" << endl;
+	cout << "   -> Declination range = [" + to_string(range.lowerDec) + ", " + to_string(range.upperDec) + "]" << endl;
+}
+
+possibleSunInfo considerPossibleSuns(int step, searchRange range) {
+	printCorrelationParameters(step, range);
 	double pearsonCoefficient;
 	int i = 0;
 	possibleSunInfo bestSun;
 	bestSun.coefficient = -23;
 	bestSun.location = "salu2";
-	for (int dec = -90; dec <= 90; dec += step) {
+	for (int dec = range.lowerDec; dec <= range.upperDec; dec += step) {
 		if (dec != -90 and dec != 90) {
-			for (int ra = 0; ra <= 360; ra += step) {
+			for (int ra = range.lowerRa; ra <= range.upperRa; ra += step) {
 				pearsonCoefficient = mainfortran_(&ra, &dec, &writeData);
-				cout << "\r" << "[" << ++i << " possible Suns considered]";
+				cout << "\r" << "[Computing: " << ++i << " possible Suns considered]";
 				if (pearsonCoefficient > bestSun.coefficient) {
 					bestSun.coefficient = pearsonCoefficient;
+					bestSun.ra = ra;
+					bestSun.dec = dec;
 					bestSun.location = "[ra=" + to_string(ra) + ", dec=" + to_string(dec) + "]";
 				}
 				// cout << "Generated file for: ra=" << ra << " dec=" << dec << " Pearson coefficient rxy = " << pearsonCoefficient << endl;
@@ -87,26 +110,48 @@ void considerPossibleSuns(int step) {
 			// cout << "Generated file for: ra=" << ra << " dec=" << dec << " Pearson coefficient rxy = " << pearsonCoefficient << endl;
 		}
 	}
-	cout << endl << "[C++: Results]" << endl;
-	cout << "   -> Largest correlation coefficient: " << bestSun.coefficient << endl;
-	cout << "   -> Estimated Sun's location: " << bestSun.location << endl;
+	
+	return bestSun;
+}
+
+searchRange initRange(possibleSunInfo sun, bool defaultRange) {
+	searchRange range;
+	if (defaultRange) {
+		range.lowerRa = 0;
+		range.upperRa = 360;
+		range.lowerDec = -180;
+		range.upperDec = 180;
+	}
+	else {
+		int raRange = 10; // 5 y 5 da mal resultado, asi da bien
+		int decRange = 10;
+		range.lowerRa = sun.ra - raRange >= 0 ? sun.ra - raRange : 0;
+		range.upperRa = sun.ra + raRange <= 360 ? sun.ra + raRange : 360;
+		range.lowerDec = sun.dec - decRange >= -180 ? sun.dec - decRange : -180;
+		range.upperDec = sun.dec + decRange <= 180 ? sun.dec + decRange : 180;
+	}
+	return range;
 }
 
 void TraverseGlobe::decreasingSTEP() {
 	vector<int> steps = {100,60,30,10,5,2,1};
 	int step;
+	possibleSunInfo bestSun;
+	searchRange range = initRange(bestSun, true);
 	for (unsigned int i = 0; i < steps.size(); ++i) {
 		step = steps[i];
 		chronoStart();
-		considerPossibleSuns(step);
+		bestSun = considerPossibleSuns(step, range);
+		printCorrelationResults(bestSun);
 		chronoEnd();
+		range = initRange(bestSun, false);
+		
 	}
 }
 
 void TraverseGlobe::initialMeasures() {
 	cout << "salu2" << endl;
 }
-
 
 void TraverseGlobe::test(float epoch) {
 	decreasingSTEP();
