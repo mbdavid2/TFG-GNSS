ac618d0245be797c9c8a9641ee24ae65db2228d1
mbdavid2 <mbdavid297@gmail.com>
2019-06-01 18:14:08 +0200

    [C] Automation for the file filtering done

--- Code/SunPositionAlgorithm/data/filterTiTimeRange.sh
+++ Code/SunPositionAlgorithm/data/filterTiTimeRange.sh
@@ -4,22 +4,65 @@
 
 
 
+# strings=(
+#     '2003.301,36000,41400'
+#     '2011.216.13908 '
+# 	'2011.210.44134 '
+	# '2003.308.71000-71100 '
+	# '2005.020.24200-24400  '
+	# '2006.340.67300 - 67500'
+	# '2001.334.3700-4000 '
+	# '2001.347.51800-52200 '
+	# '2002.196.72240 '
+	# '2004.204.27800-28000 '
+	# '2004.310.41370 '
+	# '2004.313.52500 '
+	# '2005.258.30990 '
+	# '2012.066.4400-4700 '
+	# '2012.130.50600-51000 '
+	# '2012.297.11600-12000 '
+	# '2013.310.35970 '
+# )
+
 strings=(
-    'ti.2003.301.gz,10,11.5'
+	'2011.216, 13908'
+	'2003.301,36000,41400'
+	'2011.210,44134'
+	'2003.308,71000,71100'
+	'2005.020,24200,24400'
+	'2006.340,67300,67500'
+	'2001.334,3700,4000'
+	'2001.347,51800,52200'
+	'2002.196,72240'
+	'2004.204,27800-28000'
+	'2004.310,41370'
+	'2004.313,52500'
+	'2005.258,30990'
+	'2012.066,4400,4700'
+	'2012.130,50600,51000'
+	'2012.297,11600,12000'
+	'2013.310,35970'
 )
+
+
 for i in "${strings[@]}"; do
     dataInfo="$i"
     # Split
 	arrayInfo=(${dataInfo//,/ })
+	if [ ${#arrayInfo[@]} = 2 ]; then
+		let lowerLimit="${arrayInfo[1]}"-1800
+		let upperLimit="${arrayInfo[1]}"+1800
+	else
+		let lowerLimit="${arrayInfo[1]}"
+		let upperLimit="${arrayInfo[2]}"
+	fi
 
 	# Save each parameter
 	tiDataFile=${arrayInfo[0]}
-	lowerLimit=${arrayInfo[1]}
-	upperLimit=${arrayInfo[2]}
 
 	# Filter the file
-	tiDataFileOut=${tiDataFile::-2}
-	outputFileName="$tiDataFileOut$lowerLimit""-""$upperLimit"
+	# tiDataFile=${tiDataFile::-2}
+	outputFileName="ti.""$tiDataFile"".""$lowerLimit""-""$upperLimit"".gz"
 	echo $outputFileName
 	# zcat "$tiDataFile" | gawk -v lowerLim="$lowerLimit" -v upperLim="$upperLimit" '{/a/; if ($3 >= lowerLim && $3 <= upperLim) {print $0;}}' > $outputFileName
 	# gzip $outputFileName
